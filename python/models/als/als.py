b32827da211d3f265a11d906a0e47a99d5e3ba97
Peter Henry <phenryextra@zoho.com>
2014-05-08 11:56:57 -0700

    switch which matrix is transposed and commented out heavy part

--- python/models/als/als.py
+++ python/models/als/als.py
@@ -4,8 +4,8 @@ from nflix_io import *
 
 
 if __name__ == '__main__':
-    U = load_mm("/shared/out/als/base_U.mm")
-    V = load_mm("/shared/out/als/base_V.mm")
+    #U = load_mm("/shared/out/als/base_U.mm")
+    #V = load_mm("/shared/out/als/base_V.mm")
 
     U = np.zeros((5, 2))
     V = np.zeros((10, 2))
@@ -14,15 +14,18 @@ if __name__ == '__main__':
     print U
     print U.size
     print U.shape
+    print ''
 
     print "V:"
     print V
     print V.size
     print V.shape
+    print ''
 
-    R = np.multiply(U, V.T)
+    R = np.multiply(U.T, V)
 
     print "R:"
     print R
     print R.size
     print R.shape
+    print ''
