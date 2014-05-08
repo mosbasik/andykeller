f7908332d91a0b23e819cebc56993971646dc009
Peter Henry <phenryextra@zoho.com>
2014-05-08 11:55:19 -0700

    testing with smaller matrices

--- python/models/als/als.py
+++ python/models/als/als.py
@@ -7,16 +7,22 @@ if __name__ == '__main__':
     U = load_mm("/shared/out/als/base_U.mm")
     V = load_mm("/shared/out/als/base_V.mm")
 
+    U = np.zeros((5, 2))
+    V = np.zeros((10, 2))
+
     print "U:"
+    print U
     print U.size
     print U.shape
 
     print "V:"
+    print V
     print V.size
     print V.shape
 
     R = np.multiply(U, V.T)
 
     print "R:"
+    print R
     print R.size
     print R.shape
