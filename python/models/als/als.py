9379bf9e57cfe46e3072ef725f39185ebd182f55
Peter Henry <phenryextra@zoho.com>
2014-05-08 12:04:18 -0700

    put real matrices in

--- python/models/als/als.py
+++ python/models/als/als.py
@@ -4,11 +4,11 @@ from nflix_io import *
 
 
 if __name__ == '__main__':
-    #U = load_mm("/shared/out/als/base_U.mm")
-    #V = load_mm("/shared/out/als/base_V.mm")
+    U = load_mm("/shared/out/als/base_U.mm")
+    V = load_mm("/shared/out/als/base_V.mm")
 
-    U = np.zeros((5, 2))
-    V = np.zeros((10, 2))
+    #U = np.zeros((5, 2))
+    #V = np.zeros((10, 2))
 
     print "U:"
     print U
