72561e7154f98f9c72465e6ed04c81cdbbf10f8e
Peter Henry <phenryextra@zoho.com>
2014-05-08 11:20:21 -0700

    renamed load_feature_mm() to load_mm().  added als.py

---
+++ python/models/als/als.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append('/home/phenry/andykeller')
+from nflix_io import *
+
+
+if __name__ == '__main__':
+    U = load_mm("/shared/out/als/base_U.mm")
+    V = load_mm("/shared/out/als/base_v.mm")
+
+    print "U:"
+    print U.size
+    print U.shape
+
+    print "V"
+    print V.size
+    print V.shape
