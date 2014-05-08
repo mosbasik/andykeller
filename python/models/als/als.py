6034fdb954f3c258a317bc2f85089cd06afda597
Peter Henry <phenryextra@zoho.com>
2014-05-08 12:08:59 -0700

    transfer array to matrix and save matrix to file before calculating

--- python/models/als/als.py
+++ python/models/als/als.py
@@ -4,8 +4,18 @@ from nflix_io import *
 
 
 if __name__ == '__main__':
+
+    # Get the data from the output of graphchi
     U = load_mm("/shared/out/als/base_U.mm")
     V = load_mm("/shared/out/als/base_V.mm")
+
+    # Transform from array to matrix
+    U = np.matrix(U)
+    V = np.matrix(V)
+
+    # Save matrices to files
+    np.save("Umatrix", U)
+    np.save("Vmatrix", V)
 
     #U = np.zeros((5, 2))
     #V = np.zeros((10, 2))
